b8e7b63b58d046897d2818ed76f317cfa76b7255
heyquentin <quentingauthier@gmail.com>
2022-05-04 17:02:05 -0700

    Update RDPassSpray.py

--- RDPassSpray.py
+++ RDPassSpray.py
@@ -1,6 +1,7 @@
 # HEY! So I needed a RDP paswword sprayer for some CTFs and I found this one by https://github.com/xFreed0m
 # https://github.com/xFreed0m/RDPassSpray
-# It's a cool and useful script but there's a couple things I wanted to change like colours
+# It's a cool and useful script but there's a couple things I wanted to change like colours and the logging
+# Not gonna lie, it could definitely use some work.
 # Also, I found that getting the hostnamectl part working to be a little painful. I actually tried removing the functionality but didn't like the end result
 # My suggestion to get it working is to add the suid bit to hostnamectl
 # sudo chmod u+s /usr/bin/hostnamectl
